f5a00e95564ec66edccaf85126ffd1e5de0dd3d9
Tomas Oh <tomasoh@csu.fullerton.edu>
2024-10-23 15:46:59 -0700

    add 131 files

--- FinalExam/README.hpp
+++
@@ -1,7 +0,0 @@
-/*
-Name: Tomas Oh
-Submission of my Final Project: 5/16/2023
-
-All of the graphs used in the PDF can be found near the bottom of the output.ods file (past all the rows with data).
-That's pretty much what I wanted to clarify. Thank you for reading!
-*/
